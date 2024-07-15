8524e95873323b7c7f1a008b8fef3439ccd7bf53
FloweryWisdom <srgerardoflorencio@gmail.com>
2024-07-14 23:58:02 -0600

    Pushing any changes that were made today.

---
+++ Lesson-8-DOM-Document-Object-Model/js/main.js
@@ -0,0 +1,18 @@
+// D O C U M E N T   O B J E C T   M O D E L
+
+
+let koders = ["Azael", "May", "Christian", "Manuel", "Eddy",];
+
+/*DOM elements creation Methods */
+
+
+// 1. createElement uses the name of the element as the argument
+let koderListItem = document.createElement("li");
+
+//2. createTextNode uses a string as the argument
+let koderItemText = document.createTextNode("Hello World!");
+
+//3. appendchild adds an element or node to the argument
+
+console.log(koderListItem);
+console.log(koderItemText);
